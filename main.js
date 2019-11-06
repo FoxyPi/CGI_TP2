19a5877360eb968c2835751915498824de2a2236
dmsapereira <david.pinguim2@hotmail.com>
2019-11-06 17:46:59 +0000

    Falta o SuperQuadric and we're golden

--- main.js
+++ main.js
@@ -12,17 +12,17 @@ var ryValue;
 var theta;
 var gamma;
 var mView;
-var modelMemory = 
-{ "ortho" : null, "axo" : null, "oblique" : null, "perspective": null};
+var currentView = "axo";
 
 var defaultLookAtMatrix =
 { "at" : [0,0,0], "eye" : [0,0,0], "up" : [0,1,0]};
 
-var axonAngles;
+var viewMemory = 
+{ "ortho" : orthographicView(0,0), "axo" : orthographicView(toAxonometric(42)), "oblique" : obliqueView(1, radians(30)), "perspective": perspectiveView(2)};
+
+
 var freeAxonActive = false;
 var freeObliqueActive = false;
-var alphaOblique;
-var lOblique;
 
 function $(x){
     return document.getElementById(x);
@@ -31,8 +31,8 @@ function $(x){
 function canvasSetup(canvas){
     xScale = canvas.width / window.innerWidth;
     canvas.width = window.innerWidth;
-    yScale = canvas.height / (window.innerHeight - 100);
-    canvas.height = window.innerHeight - 100;
+    yScale = canvas.height / (window.innerHeight - 300);
+    canvas.height = window.innerHeight - 300;
 }
 
 window.onload = function(){
@@ -71,13 +71,13 @@ window.onload = function(){
     mProjection = ortho(-2,2,-2,2,-10,10);
     mProjection = mult(scalem(xScale,yScale,1), mProjection); 
 
-    filled = true;
+    filled = false;
 
     $("dimetric").click();
     
-    gl.isEnabled(gl.CULL_FACE) ? gl.disable(gl.CULL_FACE) : gl.enable(gl.CULL_FACE);
-    gl.cullFace(gl.BACK);
-    gl.frontFace(gl.CCW)
+    //gl.isEnabled(gl.CULL_FACE) ? gl.disable(gl.CULL_FACE) : gl.enable(gl.CULL_FACE);
+    //gl.cullFace(gl.BACK);
+    //gl.frontFace(gl.CCW)
     render();
 }
 
@@ -95,6 +95,8 @@ function render(){
 }
 
 function setupButtonsAndSliders(){
+
+    //MODEL BUTTONS
     $("cubeButton").onclick = function(){
         cubeInit(gl);
         currentMModel = mat4();
@@ -129,118 +131,147 @@ function setupButtonsAndSliders(){
         draw_function = torusDraw;
     }
 
+
+    //ORTHO
+
+
     $("mainElevation").onclick = function(){
         rxValue = 0;
         ryValue = 0;
 
-        orthographicView(rxValue,ryValue);
+        mView = orthographicView(rxValue,ryValue);
     }
 
     $("plan").onclick = function(){
         rxValue = 90;
         ryValue = 0;
-        orthographicView(rxValue,ryValue);
+        mView = orthographicView(rxValue,ryValue);
     }
     
     $("rightElevation").onclick = function(){
         rxValue = 0;
         ryValue = 90;
 
-        orthographicView(rxValue,ryValue);
+        mView = orthographicView(rxValue,ryValue);
     }
 
-    $("isometric").onclick = function(){
-        axonAngles = toAxonometric(30, 30);
 
-        orthographicView(axonAngles[0],axonAngles[1]);
+    //AXO
+    $("isometric").onclick = function(){
+        if(freeAxonActive)
+            $("freeAxon").click();
+
+        let axonAngles = toAxonometric(30, 30);
+
+        mView = orthographicView(axonAngles[0],axonAngles[1]);
     };
 
     $("dimetric").onclick = function(){
-        axonAngles = toAxonometric(42, 7);
+        if(freeAxonActive)
+            $("freeAxon").click();
 
-        orthographicView(axonAngles[0],axonAngles[1]);
+        let axonAngles = toAxonometric(42, 7);
+
+        mView =  orthographicView(axonAngles[0],axonAngles[1]);
     };
 
     $("trimetric").onclick = function() {
-        axonAngles = toAxonometric(54.16, 23.16);
+        if(freeAxonActive)
+            $("freeAxon").click();
 
-        orthographicView(axonAngles[0],axonAngles[1]);
+        let axonAngles = toAxonometric(54.16, 23.16);
+
+        mView = orthographicView(axonAngles[0],axonAngles[1]);
     };
 
 
     $("freeAxon").onclick = function(){   
         if(!freeAxonActive){
-            orthographicView(axonAngles[0],axonAngles[1]);
-            freeAxonActive = true;
+            mView = orthographicView(axonAngles[0],axonAngles[1]);
+
+            $("alphaSlider").disabled = false;
+            $("betaSlider").disabled = false;
             
             $("activeFreeAxonTag").innerHTML = "Active"
-            $("alphaSlider").value = "0";
-            $("betaSlider").value = "0";
+
+            $("alphaSlider").value = 0;
+            $("betaSlider").value = 0;
+
+            $("alphaDisplay").value = 0;
+            $("betaDisplay").value = 0;
         }
         else{
             $("activeFreeAxonTag").innerHTML = "Inactive"
-            freeAxonActive = false;
+
+            $("alphaSlider").disabled = true;
+            $("betaSlider").disabled = true;
         }
+        freeAxonActive = !freeAxonActive;
     }
 
     $("alphaSlider").oninput = function(event){
-        axonAngles[0] = parseFloat(event.target.value);
-        orthographicView(axonAngles[0],axonAngles[1]);
-        
-        console.log(axonAngles[0])
+        if(freeAxonActive){
+            mView = orthographicView(event.target.value ,$("betaSlider").value);
+            $("alphaDisplay").value = event.target.value;
+        }
     }
 
     $("betaSlider").oninput = function(event){
-        axonAngles[1] = parseFloat(event.target.value);
-        orthographicView(axonAngles[0],axonAngles[1]);
-
-        console.log(axonAngles[1])
+        if(freeAxonActive){
+            mView =  orthographicView($("alphaSlider").value, event.target.value);
+            $("betaDisplay").value = event.target.value;
+        }
     }
-    /*$("isometric").onclick = (x => toAxonometric(30, 30));
-
-    $("dimetric").onclick = (x => toAxonometric(42, 7));
-
-    $("trimetric").onclick = (x => toAxonometric(54.16, 23.16));
-
-    $("freeAxon").onclick = (x => toAxonometric($("alpha").value, $("beta").value));
-    */
 
 
+    //OBLIQUE
     $("cavalier").onclick = function(){
-        lOblique = 1;
-        alphaOblique = radians(30);
-
-        obliqueView(lOblique,alphaOblique)
+        if(freeObliqueActive)
+            $("freeOblique").click();
+        mView = obliqueView(1,radians(30));
     }
 
     $("cabinet").onclick = function(){
-        lOblique = 0.5;
-        alphaOblique = radians(30);
-
-        obliqueView(lOblique,alphaOblique)
+        if(freeObliqueActive)
+            $("freeOblique").click();
+        mView = obliqueView(0.5,radians(30));
     }
 
     $("freeOblique").onclick = function(){
         if(!freeObliqueActive){
-            freeObliqueActive = true;
-            
+            mView = obliqueView($("lSlider").value, $("alphaObliqueSlider").value);
+            $("alphaObliqueSlider").disabled = false;
+            $("lSlider").disabled = false;
+
             $("activeFreeObliqueTag").innerHTML = "Active"
-            $("lSlider").value = "0.5";
-            $("alphaObliqueSlider").value = "0";
+
+            $("lSlider").value = 0.5;
+            $("alphaObliqueSlider").value = 0;
+
+            $("lDisplay").value = 0.5;
+            $("alphaDisplayOblique").value = 0;
         }
         else{
             $("activeFreeObliqueTag").innerHTML = "Inactive"
-            freeObliqueActive = false;
+
+            $("alphaObliqueSlider").disabled = true;
+            $("lSlider").disabled = true;
         }
+
+        freeObliqueActive = !freeObliqueActive;
     }
 
     $("lSlider").oninput = function(event){
-        obliqueView(parseFloat(event.target.value),alphaOblique);
+        mView = obliqueView(event.target.value,$("alphaObliqueSlider").value);
     }
 
     $("alphaObliqueSlider").oninput = function(event){
-        alphaOblique = radians(parseFloat(event.target.value))
-        obliqueView(lOblique, alphaOblique);
+        mView = obliqueView($("lSlider").value, radians(event.target.value));
+    }
+    //PERSPECTIVE
+    $("dSlider").oninput = function(event){
+        mView = perspectiveView(event.target.value);
+        $("dDisplay").value = event.target.value;
     }
 }
 
@@ -251,17 +282,6 @@ function degrees(radians) {
 function toAxonometric(a, b){
     let theta = Math.atan(Math.sqrt(Math.tan(radians(a))/Math.tan(radians(b)))) - Math.PI/2;
     let epsilon = Math.asin(Math.sqrt(Math.tan(radians(a) * Math.tan(radians(b)))));
-    console.log("Epsilon is ", degrees(epsilon));
-    console.log("Theta is ", degrees(theta)); 
-    
-    
-    let r1 = Math.cos(epsilon);
-    let r2 = Math.cos(theta) / Math.cos(radians(b));
-    let r3 = -Math.sin(theta) / Math.cos(radians(a));
-    console.log("R1 is ",r1);
-    console.log("R2 is ",r2);
-    console.log("R3 Is ",r3);
-    
 
             //rxValue        //ryValue
     return [degrees(epsilon),degrees(theta)];
@@ -295,6 +315,29 @@ function openTab(event,tabId){
     var i, tablinks, tabContents;
 
     tabContents = document.getElementsByClassName("tabcontent");
+
+    //MEMORY
+    viewMemory[currentView] = mView;
+    switch(event.target.id){
+        case "orthoButton":
+            currentView = "ortho";
+            break;
+        case "axonButton":
+            currentView = "axo";
+            break;
+        case "obliqueButton":
+            currentView = "oblique";
+            break;
+        case "perspectiveButton":
+            currentView = "perspective";
+            break;
+        default:
+            alert("Unexpected Button");
+    }
+
+    mView = viewMemory[currentView];
+
+    //TAB FUNCTIONALITY
     for(i = 0; i < tabContents.length;i++)
         tabContents[i].style.display = "none";
 
@@ -309,15 +352,24 @@ function openTab(event,tabId){
 }
 
 function orthographicView(rxValue,ryValue){
-    mView = lookAt(defaultLookAtMatrix.eye, defaultLookAtMatrix.at, defaultLookAtMatrix.up);
-    mView = mult(rotateY(ryValue), mView);
-    mView = mult(rotateX(rxValue), mView);
+    var view = lookAt(defaultLookAtMatrix.eye, defaultLookAtMatrix.at, defaultLookAtMatrix.up);
+    view = mult(rotateY(ryValue), view);
+    view = mult(rotateX(rxValue), view);
+    return view;
 }
 
 function obliqueView(l, alpha){
-    mView = lookAt(defaultLookAtMatrix.eye, defaultLookAtMatrix.at, defaultLookAtMatrix.up);
-    mView = mult(mat4([1,0,(-l * Math.cos(alpha)),0],
+    var view = lookAt(defaultLookAtMatrix.eye, defaultLookAtMatrix.at, defaultLookAtMatrix.up);
+    view = mult(mat4([1,0,(-l * Math.cos(alpha)),0],
                       [0,1,(-l * Math.sin(alpha)),0],
                       [0,0,1,0],
-                      [0,0,0,1]), mView);
+                      [0,0,0,1]), view);
+    return view;
+}
+
+function perspectiveView(d){
+    return mat4([1,0,0,0],
+                [0,1,0,0],
+                [0,0,1,0],
+                [0,0,1/d,1]);
 }
