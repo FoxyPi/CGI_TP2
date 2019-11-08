67164e688436c77fa501e108db98383e45662fce
FoxyPi <filipemnjose@gmail.com>
2019-11-08 10:57:35 +0000

    VIM NOCODIGO

--- main.js
+++ main.js
@@ -404,23 +404,23 @@ class Superquadric{
         this.nlon = nlon | 30;
         this.e1 = e1;
         this.e2 = e2;
-        sphereBuild(nlat, nlon);
-        sphereUploadData(gl);
+        superBuild();
+        superUploadData(gl);
     }
     // Generate points using polar coordinates
-     sphereBuild(nlat, nlon){
+     superBuild(){
         // phi will be latitude
         // theta will be longitude
     
-        let d_phi = Math.PI / (nlat+1);
-        let d_theta = 2*Math.PI / nlon;
+        let d_phi = Math.PI / (this.nlat+1);
+        let d_theta = 2*Math.PI / this.nlon;
         let r = 0.5;
         let x,y,z;
 
         
         // Generate middle
-        for(var i=0, phi=Math.PI/2-d_phi; i<nlat; i++, phi-=d_phi) {
-            for(var j=0, theta=0; j<nlon; j++, theta+=d_theta) {
+        for(var i=0, phi=Math.PI/2-d_phi; i<this.nlat; i++, phi-=d_phi) {
+            for(var j=0, theta=0; j<this.nlon; j++, theta+=d_theta) {
                 x = r * Math.pow(Math.cos(phi), e1) * Math.pow(Math.cos(theta), e2);
                 y = r * Math.pow(Math.sin(phi), e1);
                 z = r * Math.pow(Math.cos(phi), e1) * Math.pow(Math.sin(theta), e2);
@@ -439,14 +439,14 @@ class Superquadric{
         // Generate the faces
         
         // north pole faces
-        for(var i=0; i<nlon-1; i++) {
+        for(var i=0; i<this.nlon-1; i++) {
             this.super_faces.push(0);
             this.super_faces.push(i+2);
             this.super_faces.push(i+1);
         }
         this.super_faces.push(0);
         this.super_faces.push(1);
-        this.super_faces.push(nlon);
+        this.super_faces.push(this.nlon);
         
         // general middle faces
         var offset=1;
@@ -479,94 +479,94 @@ class Superquadric{
             this.super_faces.push(offset+j);
             this.super_faces.push(offset+j+1);
         }
-        this.super_faces.push(offset+nlon);
-        this.super_faces.push(offset+nlon-1);
+        this.super_faces.push(offset+this.nlon);
+        this.super_faces.push(offset+this.nlon-1);
         this.super_faces.push(offset);
     
         // Build the edges
-        for(var i=0; i<nlon; i++) {
-            sphere_edges.push(0);   // North pole 
-            sphere_edges.push(i+1);
+        for(var i=0; i<this.nlon; i++) {
+            super_edges.push(0);   // North pole 
+            super_edges.push(i+1);
         }
 
-        for(var i=0; i<nlat; i++, p++) {
-            for(var j=0; j<nlon;j++, p++) {
-                var p = 1 + i*nlon + j;
-                sphere_edges.push(p);   // horizontal line (same latitude)
-                if(j!=nlon-1) 
-                    sphere_edges.push(p+1);
-                else sphere_edges.push(p+1-nlon);
+        for(var i=0; i<this.nlat; i++, p++) {
+            for(var j=0; j<this.nlon;j++, p++) {
+                var p = 1 + i*this.nlon + j;
+                super_edges.push(p);   // horizontal line (same latitude)
+                if(j!=this.nlon-1) 
+                    super_edges.push(p+1);
+                else super_edges.push(p+1-this.nlon);
                 
-                if(i!=nlat-1) {
-                    sphere_edges.push(p);   // vertical line (same longitude)
-                    sphere_edges.push(p+nlon);
+                if(i!=this.nlat-1) {
+                    super_edges.push(p);   // vertical line (same longitude)
+                    super_edges.push(p+this.nlon);
                 }
                 else {
-                    sphere_edges.push(p);
-                    sphere_edges.push(sphere_points.length-1);
+                    super_edges.push(p);
+                    super_edges.push(super_points.length-1);
                 }
             }
         }
         
     }
 
-    function sphereUploadData(gl)
+    function superUploadData(gl)
     {
-        sphere_points_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_points_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(sphere_points), gl.STATIC_DRAW);
+        super_points_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_points_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(super_points), gl.STATIC_DRAW);
         
-        sphere_normals_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_normals_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(sphere_normals), gl.STATIC_DRAW);
+        super_normals_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_normals_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(super_normals), gl.STATIC_DRAW);
         
-        sphere_faces_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere_faces_buffer);
+        super_faces_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, super_faces_buffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.super_faces), gl.STATIC_DRAW);
         
-        sphere_edges_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere_edges_buffer);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(sphere_edges), gl.STATIC_DRAW);
+        super_edges_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, super_edges_buffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(super_edges), gl.STATIC_DRAW);
     }
 
-    function sphereDrawWireFrame(gl, program)
+    function superDrawWireFrame(gl, program)
     {    
         gl.useProgram(program);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_points_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_points_buffer);
         var vPosition = gl.getAttribLocation(program, "vPosition");
         gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_normals_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_normals_buffer);
         var vNormal = gl.getAttribLocation(program, "vNormal");
         gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vNormal);
         
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere_edges_buffer);
-        gl.drawElements(gl.LINES, sphere_edges.length, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, super_edges_buffer);
+        gl.drawElements(gl.LINES, super_edges.length, gl.UNSIGNED_SHORT, 0);
     }
 
-    function sphereDrawFilled(gl, program)
+    function superDrawFilled(gl, program)
     {
         gl.useProgram(program);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_points_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_points_buffer);
         var vPosition = gl.getAttribLocation(program, "vPosition");
         gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
         
-        gl.bindBuffer(gl.ARRAY_BUFFER, sphere_normals_buffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, super_normals_buffer);
         var vNormal = gl.getAttribLocation(program, "vNormal");
         gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vNormal);
         
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sphere_faces_buffer);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, super_faces_buffer);
         gl.drawElements(gl.TRIANGLES, this.super_faces.length, gl.UNSIGNED_SHORT, 0);
     }
 
-    function sphereDraw(gl, program, filled=false) {
-        if(filled) sphereDrawFilled(gl, program);
-        else sphereDrawWireFrame(gl, program);
+    function superDraw(gl, program, filled=false) {
+        if(filled) superDrawFilled(gl, program);
+        else superDrawWireFrame(gl, program);
     }
 }
